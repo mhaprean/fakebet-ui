f862152c541e5b21e0fbf8226730584f8103543b
mhaprean <marianhaprean@gmail.com>
2023-01-03 17:05:25 +0200

    validate total goals

--- src/services/igubetValidator.ts
+++ src/services/igubetValidator.ts
@@ -6,7 +6,7 @@ const validate1x2 = (market: IIgubetMarket, periods: IOddspediaMatchInfoPeriods)
   const awayScore = periods[0].away + periods[1].away;
 
   const outcomes = market.outcomes.map((outcome, idx) => {
-    const newOutcome = { ...outcome, is_validated: true, is_winner: false };
+    const newOutcome: IOutcome = { ...outcome, is_validated: true, is_winner: false };
 
     if (outcome.id === 31758 && homeScore > awayScore) {
       newOutcome.is_winner = true;
@@ -34,13 +34,79 @@ const validateBothTeamsToScore = (
   const awayScore = periods[0].away + periods[1].away;
 
   const outcomes = market.outcomes.map((outcome, idx) => {
-    const newOutcome = { ...outcome, is_validated: true, is_winner: false };
+    const newOutcome: IOutcome = { ...outcome, is_validated: true, is_winner: false };
 
     if (outcome.id === 1449 && homeScore > 0 && awayScore > 0) {
       newOutcome.is_winner = true;
     }
 
     if (outcome.id === 1450 && (homeScore === 0 || awayScore === 0)) {
+      newOutcome.is_winner = true;
+    }
+
+    return newOutcome;
+  });
+
+  return { ...market, outcomes };
+};
+
+const validateHomeTeamTotal = (market: IIgubetMarket, periods: IOddspediaMatchInfoPeriods): IIgubetMarket => {
+  const homeScore = periods[0].home + periods[1].home;
+
+  const limit = parseFloat(market.specifier.replace('total=', ''));
+
+  const outcomes = market.outcomes.map((outcome, idx) => {
+    const newOutcome: IOutcome = { ...outcome, is_validated: true, is_winner: false };
+
+    if (outcome.id === 28428 && homeScore < limit) {
+      newOutcome.is_winner = true;
+    }
+    if (outcome.id === 28429 && homeScore > limit) {
+      newOutcome.is_winner = true;
+    }
+
+    return newOutcome;
+  });
+
+  return { ...market, outcomes };
+};
+
+const validateAwayTeamTotal = (market: IIgubetMarket, periods: IOddspediaMatchInfoPeriods): IIgubetMarket => {
+  const awayScore = periods[0].away + periods[1].away;
+
+  const limit = parseFloat(market.specifier.replace('total=', ''));
+
+  const outcomes = market.outcomes.map((outcome, idx) => {
+    const newOutcome: IOutcome = { ...outcome, is_validated: true, is_winner: false };
+
+    if (outcome.id === 28579 && awayScore < limit) {
+      newOutcome.is_winner = true;
+    }
+    if (outcome.id === 28580 && awayScore > limit) {
+      newOutcome.is_winner = true;
+    }
+
+    return newOutcome;
+  });
+
+  return { ...market, outcomes };
+};
+
+const validateTotalGoals = (market: IIgubetMarket, periods: IOddspediaMatchInfoPeriods): IIgubetMarket => {
+  const homeScore = periods[0].home + periods[1].home;
+  const awayScore = periods[0].away + periods[1].away;
+
+  const total = awayScore + homeScore;
+
+  const limit = parseFloat(market.specifier.replace('total=', ''));
+
+  const outcomes = market.outcomes.map((outcome, idx) => {
+    const newOutcome: IOutcome = { ...outcome, is_validated: true, is_winner: false };
+
+    if (outcome.id === 1027 && total < limit) {
+      newOutcome.is_winner = true;
+    }
+    if (outcome.id === 1028 && total > limit) {
       newOutcome.is_winner = true;
     }
 
@@ -58,11 +124,15 @@ export const validateMarkets = (
     switch (market.id) {
       case 4761: // 1x2
         return validate1x2(market, periods);
-        break;
-
       case 391: // both teams to score
         return validateBothTeamsToScore(market, periods);
-        break;
+
+      case 219: // total goals in match
+        return validateTotalGoals(market, periods);
+      case 4043: // home team total goals
+        return validateHomeTeamTotal(market, periods);
+      case 4081: // away team total goals
+        return validateAwayTeamTotal(market, periods);
       default:
         break;
     }
