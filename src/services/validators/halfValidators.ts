33a14aced9eb172bb44227f46dc522de39edda25
mhaprean <marianhaprean@gmail.com>
2023-01-04 21:41:49 +0200

    validate double chance for both halves

--- src/services/validators/halfValidators.ts
+++ src/services/validators/halfValidators.ts
@@ -52,6 +52,68 @@ const validateSecondHalf1x2 = (market: IIgubetMarket, periods: IOddspediaMatchIn
   return { ...market, outcomes };
 };
 
+const validateFirstHalfDoubleChance = (
+  market: IIgubetMarket,
+  periods: IOddspediaMatchInfoPeriods
+): IIgubetMarket => {
+  const homeScore = periods[0].home;
+  const awayScore = periods[0].away;
+
+  const outcomes = market.outcomes.map((outcome, idx) => {
+    const newOutcome: IOutcome = { ...outcome, is_validated: true, is_winner: false };
+
+    // 1x
+    if (outcome.id === 1084 && homeScore >= awayScore) {
+      newOutcome.is_winner = true;
+    }
+
+    // 12
+    if (outcome.id === 1085 && homeScore !== awayScore) {
+      newOutcome.is_winner = true;
+    }
+
+    // x2
+    if (outcome.id === 1086 && homeScore <= awayScore) {
+      newOutcome.is_winner = true;
+    }
+
+    return newOutcome;
+  });
+
+  return { ...market, outcomes };
+};
+
+const validateSecondHalfDoubleChance = (
+  market: IIgubetMarket,
+  periods: IOddspediaMatchInfoPeriods
+): IIgubetMarket => {
+  const homeScore = periods[1].home;
+  const awayScore = periods[1].away;
+
+  const outcomes = market.outcomes.map((outcome, idx) => {
+    const newOutcome: IOutcome = { ...outcome, is_validated: true, is_winner: false };
+
+    // 1x
+    if (outcome.id === 1451 && homeScore >= awayScore) {
+      newOutcome.is_winner = true;
+    }
+
+    // 12
+    if (outcome.id === 1452 && homeScore !== awayScore) {
+      newOutcome.is_winner = true;
+    }
+
+    // x2
+    if (outcome.id === 1453 && homeScore <= awayScore) {
+      newOutcome.is_winner = true;
+    }
+
+    return newOutcome;
+  });
+
+  return { ...market, outcomes };
+};
+
 export const validateHalfMarkets = (
   markets: IIgubetMarket[],
   periods: IOddspediaMatchInfoPeriods
@@ -62,6 +124,12 @@ export const validateHalfMarkets = (
         return validateFirstHalf1x2(market, periods);
       case 4050: // 2nd half 1x2'
         return validateSecondHalf1x2(market, periods);
+
+      case 231: // 1st half double chance
+        return validateFirstHalfDoubleChance(market, periods);
+
+      case 392: // 2nd half double chance
+        return validateSecondHalfDoubleChance(market, periods);
       default:
         break;
     }
