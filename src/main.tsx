5d22aa0b87242cca9fed9afa5b80daaabcb6ff6c
mhaprean <marianhaprean@gmail.com>
2023-01-11 13:54:08 +0200

    add redux persist to the app

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,31 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import igubetApi from './features/igubetApi';
+import iguDetaAuthSlice from './features/iguDetaAuthSlice';
 import oddspediaApi from './features/oddspediaApi';
+
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import iguDetaApi from './features/iguDetaApi';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: storage,
+};
+
+const persistedIguAuth = persistReducer(persistConfig, iguDetaAuthSlice);
 
 export const store = configureStore({
   reducer: {
     [oddspediaApi.reducerPath]: oddspediaApi.reducer,
     [igubetApi.reducerPath]: igubetApi.reducer,
+    [iguDetaApi.reducerPath]: iguDetaApi.reducer,
+    igudetaauth: persistedIguAuth,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat([oddspediaApi.middleware, igubetApi.middleware]),
+    }).concat([oddspediaApi.middleware, igubetApi.middleware, iguDetaApi.middleware]),
 });
 
 export type AppDispatch = typeof store.dispatch;
