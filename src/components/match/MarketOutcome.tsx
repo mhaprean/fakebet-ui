f1219b3c8ce2b0bf7ff986ed593511735eb2be34
mhaprean <marianhaprean@gmail.com>
2023-01-18 15:23:57 +0200

    extract market outcome to single component

---
+++ src/components/match/MarketOutcome.tsx
@@ -0,0 +1,61 @@
+import { Button, Typography } from '@mui/material';
+import { styled, alpha } from '@mui/material/styles';
+import classNames from 'classnames';
+import { IOutcome } from '../../redux/features/igubetTypes';
+
+const StyledMarketOutcome = styled(Button)`
+  display: flex;
+  justify-content: space-between;
+  flex-basis: 30%;
+  flex-grow: 1;
+  min-width: 130px;
+  background: ${(props) => props.theme.palette.background.default};
+  border-radius: 5px;
+  text-transform: unset;
+  border: 1px solid ${(props) => props.theme.palette.divider};
+
+  &.winner {
+    background: ${(props) => alpha(props.theme.palette.primary.main, 0.2)};
+  }
+
+  &.selected {
+    background: ${(props) => props.theme.palette.info.light};
+  }
+
+  .outcome-name {
+    color: ${(props) => props.theme.palette.text.secondary};
+  }
+  .outcome-value {
+    font-weight: ${(props) => props.theme.typography.fontWeightMedium};
+    color: ${(props) => props.theme.palette.text.primary};
+  }
+`;
+
+interface IPropsMarketOutcome {
+  outcome: IOutcome;
+  onSelect?: (outcome: IOutcome) => void;
+  isSelected?: boolean;
+}
+
+const MarketOutcome = ({ outcome, onSelect = () => {}, isSelected = false }: IPropsMarketOutcome) => {
+  return (
+    <StyledMarketOutcome
+      className={classNames('MarketOutcome', {
+        winner: outcome.is_winner,
+        selected: isSelected,
+      })}
+      key={outcome.id}
+      onClick={() => onSelect(outcome)}
+    >
+      <Typography className="outcome-name" variant="body2">
+        {outcome.formated_name}
+        {/* {outcome.id} */}
+      </Typography>
+      <Typography className="outcome-value" variant="body2">
+        {outcome.formated_value}
+      </Typography>
+    </StyledMarketOutcome>
+  );
+};
+
+export default MarketOutcome;
--- src/components/match/MarketOutcomes.tsx
+++ src/components/match/MarketOutcomes.tsx
@@ -2,6 +2,7 @@ import { Button, Typography } from '@mui/material';
 import { alpha, styled } from '@mui/material/styles';
 import classNames from 'classnames';
 import { IOutcome } from '../../redux/features/igubetTypes';
+import MarketOutcome from './MarketOutcome';
 
 interface IPropsMarketOutcomes {
   outcomes: IOutcome[];
@@ -13,62 +14,13 @@ const StyledMarketOutcomes = styled('div')`
   gap: 10px;
   padding: 10px 0;
   margin-bottom: 20px;
-
-  .outcome {
-    display: flex;
-    justify-content: space-between;
-    flex-basis: 30%;
-    flex-grow: 1;
-    min-width: 130px;
-    background: ${(props) => props.theme.palette.background.default};
-    border-radius: 5px;
-    text-transform: unset;
-    border: 1px solid ${(props) => props.theme.palette.divider};
-
-    &.winner {
-      background: ${(props) => alpha(props.theme.palette.primary.main, 0.2)};
-    }
-
-    &.selected {
-      background: ${(props) => props.theme.palette.info.light};
-    }
-
-    .outcome-name {
-      color: ${(props) => props.theme.palette.text.secondary};
-    }
-    .outcome-value {
-      font-weight: ${(props) => props.theme.typography.fontWeightMedium};
-      color: ${(props) => props.theme.palette.text.primary};
-    }
-  }
 `;
 
 const MarketOutcomes = ({ outcomes }: IPropsMarketOutcomes) => {
-
-
-  const onSelect = (outcome: IOutcome) => {
-
-  }
-  
   return (
     <StyledMarketOutcomes className="MarketOutcomes">
-      {outcomes.map((outcome) => (
-        <Button
-          className={classNames('outcome', {
-            winner: outcome.is_winner,
-            selected: false,
-          })}
-          key={outcome.id}
-          onClick={() => onSelect(outcome)}
-        >
-          <Typography className="outcome-name" variant="body2">
-            {outcome.formated_name}
-            {/* {outcome.id} */}
-          </Typography>
-          <Typography className="outcome-value" variant="body2">
-            {outcome.formated_value}
-          </Typography>
-        </Button>
+      {outcomes.map((outcome, idx) => (
+        <MarketOutcome key={idx} outcome={outcome} />
       ))}
     </StyledMarketOutcomes>
   );
